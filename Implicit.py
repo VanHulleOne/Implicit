71a3b82ddd3b1c1e159eb9248932e01490f57ea3
VanHulleOne <vanhullel@my.uwstout.edu>
2016-05-09 17:02:46 -0500

    Now tests for sharp corners.

--- Implicit.py
+++ Implicit.py
@@ -8,8 +8,8 @@ Created on Fri Apr 29 16:00:00 2016
 import numpy as np
 import matplotlib.pyplot as plt
 from sympy import geometry as g
-from collections import Counter
 from Geom import Polygon, Point, Line
+from collections import deque
 
 # The X and Y offsets in a point
 X, Y = 0, 1
@@ -29,12 +29,13 @@ ANGLE_EPS = EPSILON
 # Cardiod
 #func = '((x-3)**2+y**2+5*(x-3))**2-5**2*((x-3)**2+y**2)'
 #Cassini oval https://en.wikipedia.org/wiki/Implicit_curve
-func = '((x**2+y**2)**2-2*5**2*(x**2-y**2)-(5**4-5**4))'
+#func = '((x**2+y**2)**2-2*5**2*(x**2-y**2)-(5**4-5**4))'
 # wavey surface
 #func = 'np.sin(x+y)-np.cos(x*y)+1'
 # Example
-#func = '5*x**3 -17.3 * y**2 + np.sin(x*y)'
-#func = '-4+(x**2+y**2)**0.5'
+func = '5*x**3 -17.3 * y**2 + np.sin(x*y)'
+# Distance
+#func = '-(-4+(x**2+y**2)**0.5)'
 
 delta = 0.5
 size = 10
@@ -42,18 +43,18 @@ size = 10
 x = np.arange(-size, size, delta)
 y = np.arange(-size, size, delta)
 
-X, Y = np.meshgrid(x, y)
+xGrid, yGrid = np.meshgrid(x, y)
 
 FN = lambda x,y: eval(func)
 
-F = FN(X,Y)
+F = FN(xGrid, yGrid)
 
-cs = plt.contour(X, Y, F, [0])
+cs = plt.contour(xGrid, yGrid, F, [0], colors = 'r')
 paths = cs.collections[0].get_paths()
 
 plt.show()
 
-print 'Number of connected components is: %d' %len(paths)
+print '\nNumber of connected components is: %d' %len(paths)
 
 prev = None
 pointList = []
@@ -66,18 +67,92 @@ for coord in paths[0].vertices:
 
 def pairwise(l1):
     l1Iter = iter(l1)
-    first = prev = next(l1Iter)
+    prev = next(l1Iter)
     for curr in l1Iter:
         yield (prev, curr)
         prev = curr
-#    yield (prev, first)
     
 poly = Polygon(pairwise(pointList))
 
-#shape = g.Polygon(*paths[0].vertices)  
+print poly.simplyConnected()
+print ''
 
+shape = g.Polygon(*paths[0].vertices)  
+csHigher = plt.contour(xGrid, yGrid, F,[delta], colors = 'g')
+pathsHigher = csHigher.collections[0].get_paths()
+shapeHigher = g.Polygon(*pathsHigher[0].vertices)
 
+if abs(shapeHigher.area) > abs(shape.area):
+    print 'The figure has an empty interior'
+else:
+    print 'The figure has a non-empty interior'
 
+print ''
+print 'The max distance from the approximations is {:.3f}'.format(np.sqrt(2*delta**2))
+print ''
 
+def quick2ndDeriv_coro(num, tolerance):
+    first = rollingAvg_coro(num)
+    next(first)
+    isSharp = False
+    for i in xrange(num):
+        temp = yield isSharp
+        first.send(temp)
+    second = rollingAvg_coro(num)
+    next(second)
+    q = deque()
+    for i in xrange(num):
+        temp = yield isSharp
+        second.send(temp)
+        q.append(temp)
+    while 1:
+        temp = yield isSharp
+        q.append(temp)
+        firstAvg = first.send(q.popleft())
+        secondAvg = second.send(temp)
+        if(abs(firstAvg-secondAvg) > tolerance):
+            isSharp = True
+            
+def rollingAvg_coro(num):
+    q = deque()
+    total = 0
+    average = None
+    for i in xrange(num):
+        temp = yield average 
+        total += temp
+        q.append(temp)
+        average = 1.0*total/len(q)        
+        
+    while 1:
+        temp = yield 1.0*total/num
+        total += temp - q.popleft()
+        q.append(temp)
 
+def sharpCorner(lines, num, tolerance):
+    sharpX = quick2ndDeriv_coro(num, tolerance)
+    next(sharpX)
+    sharpY = quick2ndDeriv_coro(num, tolerance)
+    next(sharpY)
+    for line in lines + lines[:6]:
+        normalizedSlope = line.normalizedSlope()
+        if(sharpX.send(normalizedSlope[X]) or sharpY.send(normalizedSlope[Y])):
+            return True
+    return False
     
+if sharpCorner(poly.lines, 2, 0.4):
+    print 'The shape has at least one sharp corner.'
+else:
+    print 'The shape does not have sharp corners.'
+    
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
